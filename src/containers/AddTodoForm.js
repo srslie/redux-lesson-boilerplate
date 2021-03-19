69bf6ec4152dc2db498febf8f71513abdfa4581d
srslie <aliceruppert@gmail.com>
2021-03-19 00:01:51 -0600

    Add form with connect to addToDo

---
+++ src/containers/AddTodoForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import {addToDo} from '../actions';
+import {connect} from 'react-redux';
+
+class AddTodoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todo: '' };
+  }
+
+  handleChange = (e) => {
+    this.setState({ todo: e.target.value });
+  }
+
+  submitForm = (e) => {
+    e.preventDefault()
+    this.props.addToDo(this.state.todo)
+    this.setState({ todo: '' });
+  }
+
+  render() {
+    return (
+      <section>
+        <form onSubmit={this.submitForm}>
+          <input
+            value={this.state.todo}
+            placeholder="Add A Todo"
+            onChange={this.handleChange} />
+          <button>Add Todo</button>
+        </form>
+      </section>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  addToDo: text => dispatch( addToDo(text) )
+})
+
+export default connect(null, mapDispatchToProps)(AddTodoForm);
